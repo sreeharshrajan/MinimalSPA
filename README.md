c75ac1bc2cafa72eac4ace5bdef5ea1f0b664540
Sreeharsh K <sreeharshkrajan@gmail.com>
2022-04-15 21:02:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Single Page Web Application using Vanilla JS
+
+The one framework you don't have to install. It includes routing (you can bookmark and navigate pages), databinding, reusable web components and uses JavaScript’s native module functionality.
+
+## Installation
+
+Use Visual Studio Code and install Live Server Extention to run the application.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ router.js
@@ -0,0 +1,34 @@
+// an object to hold the routes
+const routes = {
+  404: "/views/404.html",
+  "/": "/views/home.html",
+  "/about": "/views/about.html",
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  //   get current path name
+  const route = routes[path] || routes[404];
+  //   get the route from the routes object or the 404 route
+  const html = await fetch(route).then((data) => data.text());
+  // fetch current path view html
+  document.getElementById("app").innerHTML = html;
+  //   change view_container html to the fetched view html
+};
+
+handleLocation();
+// call handleLocation function
+
+const route = (event) => {
+  event = event || window.event;
+  //  get the event object
+  event.preventDefault();
+  //  prevent default behaviour
+  window.history.pushState({}, "", event.target.href);
+  //   change the url to the target href
+  handleLocation();
+  //   call handleLocation function
+};
+window.onpopstate = handleLocation;
+//   listen for popstate event
+window.route = route;
